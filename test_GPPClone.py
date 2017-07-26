8336bf713f86898d77dc75c4c2e9f4039548af59
RJ Rosario <rrosa006@ucr.edu>
2017-07-25 21:46:10 -0700

    added lines in tests to change columns from ints to strings

--- test_GPPClone.py
+++ test_GPPClone.py
@@ -8,6 +8,7 @@ def test_calculate_cv_20170720():
     calculate_cv_path = "C:/Users/RJ/Desktop/testdir/test20170720/calculate_cv.xlsx"
     calculate_cv_code = GPPCA.calculate_cv((GPPCA.add_sample_name(GPPCA.import_data(raw))))
     calculate_cv_gpp = pd.read_excel(calculate_cv_path)
+    calculate_cv_gpp.columns = calculate_cv_gpp.columns.astype(str)
     pdt.assert_almost_equal(calculate_cv_code, calculate_cv_gpp)
 
 
@@ -23,6 +24,7 @@ def test_calculate_y_transform_20170720():
     transform_y_path = "C:/Users/RJ/Desktop/testdir/test20170720/y_transform.xlsx"
     transform_y_code = GPPCA.transform_y(GPPCA.add_sample_name(GPPCA.import_data(raw)))
     transform_y_gpp = pd.read_excel(transform_y_path)
+    transform_y_gpp.columns = transform_y_gpp.columns.astype(str)
     pdt.assert_almost_equal(transform_y_code, transform_y_gpp)
 
 
@@ -34,3 +36,4 @@ def test_inhibition_coefficients():
     inhibition_coefficients_code = GPPCA.inhibition_coefficients(response, logx)
     inhibition_coefficients_gpp = pd.read_excel(inhibition_coefficients_path)
     pdt.assert_frame_equal(inhibition_coefficients_code, inhibition_coefficients_gpp)
+
