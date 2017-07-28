aa9b056758c6246b51f81307b24bb126273aeaaa
RJ Rosario <rrosa006@ucr.edu>
2017-07-27 23:10:26 -0700

    added analyze function that generates graph and tabulates coefficients.

--- GPPClone_AssayCalculations_Class.py
+++ GPPClone_AssayCalculations_Class.py
@@ -8,6 +8,7 @@ from abc import ABCMeta, abstractmethod
 class Nab (object):
     """
     Nab Analysis
+
     """
 
     __metaclass__ = ABCMeta
@@ -16,6 +17,9 @@ class Nab (object):
     end_response_column = ' '
     graph_type = ' '
     log_50 = ' '
+    x_label = ' '
+    y_label = ' '
+    title = ' '
     start_concentration = 0
     dilution_ratio = 0
     number_dilutions = 0
@@ -24,32 +28,6 @@ class Nab (object):
         self.path = path
         self.orientation = orientation
         self.data = self.add_sample_name()
-
-    def add_sample_name(self):
-        excel = pd.read_excel(self.path, header=None)
-        sample = ['S1', 'S1', 'S2', 'S2', 'S3', 'S3', 'S4', 'S4', 'S5', 'S5', 'S6', 'S6']
-        row = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-        column = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
-        row = np.array([row[i] for i in range(excel.shape[0])])
-        column = np.array([column[i] for i in range(excel.shape[1])])
-        row_index = pd.MultiIndex.from_arrays(
-            [np.array([sample[i] for i in range(excel.shape[0])]), row])
-
-        col_index = pd.MultiIndex.from_arrays(
-            [np.array([sample[i] for i in range(excel.shape[1])]), column])
-        if self.orientation == 0:
-            df = pd.DataFrame(data=excel.values, index=row_index, columns=column)
-            return df
-        if self.orientation == 1:
-            df = pd.DataFrame(data=excel.values, index=row, columns=col_index)
-            return df
-
-    def calculate_cv(self):
-        if not isinstance(self.data.index, pd.core.index.MultiIndex):
-            self.data = self.data.T
-        with np.errstate(divide='ignore', invalid='ignore'):
-            cv = self.data.std(axis=0, level=0, ddof=1) / self.data.mean(axis=0, level=0) * 100
-            return cv
 
     @staticmethod
     def get_concentrations(starting_concentration, dilution_ratio, n_dilutions, graph_type):
@@ -69,6 +47,31 @@ class Nab (object):
     @staticmethod
     def response(concentration, bottom, top, log_50, hill_slope):
         return bottom + (top - bottom) / (1 + np.power(10, ((log_50 - concentration) * hill_slope)))
+
+    def add_sample_name(self):
+        excel = pd.read_excel(self.path, header=None)
+        sample = ['S1', 'S1', 'S2', 'S2', 'S3', 'S3', 'S4', 'S4', 'S5', 'S5', 'S6', 'S6']
+        row = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+        column = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+        row = np.array([row[i] for i in range(excel.shape[0])])
+        column = np.array([column[i] for i in range(excel.shape[1])])
+        row_index = pd.MultiIndex.from_arrays(
+            [np.array([sample[i] for i in range(excel.shape[0])]), row])
+        col_index = pd.MultiIndex.from_arrays(
+            [np.array([sample[i] for i in range(excel.shape[1])]), column])
+        if self.orientation == 0:
+            df = pd.DataFrame(data=excel.values, index=row_index, columns=column)
+            return df
+        if self.orientation == 1:
+            df = pd.DataFrame(data=excel.values, index=row, columns=col_index)
+            return df
+
+    def calculate_cv(self):
+        if not isinstance(self.data.index, pd.core.index.MultiIndex):
+            self.data = self.data.T
+        with np.errstate(divide='ignore', invalid='ignore'):
+            cv = self.data.std(axis=0, level=0, ddof=1) / self.data.mean(axis=0, level=0) * 100
+            return cv
 
     def transform_y(self):
         if not isinstance(self.data.index, pd.core.index.MultiIndex):
@@ -107,8 +110,28 @@ class Nab (object):
             decimals=3)
         return coefficient_storage
 
+    def analyze(self):
+        concentrations = Nab.get_concentrations(self.start_concentration, self.dilution_ratio, self.number_dilutions,
+                                                self.graph_type)
+
+        y = Nab.transform_y(self)
+        error = Nab.error_transform_y(self)
+        fit = Nab.coefficients(self)
+
+        sns.plt.axes(xscale='log')
+        sns.plt.xlabel(self.x_label)
+        sns.plt.ylabel(self.y_label)
+        sns.plt.title(self.title)
+
+        for i in range(y.shape[0]):
+            sns.plt.errorbar(concentrations, y.values[i], yerr=error.values[i], fmt='-o', label=('S' + str(i + 1)))
+        sns.plt.legend()
+        sns.plt.table(cellText=fit.values, colWidths=[0.25] * len(fit.columns), rowLabels=fit.index,
+                      colLabels=fit.columns,
+                      cellLoc='center', rowLoc='center', loc='bottom')
+
     @abstractmethod
-    def graph(self):
+    def type(self):
         pass
 
 
@@ -120,8 +143,11 @@ class DoseResponse(Nab):
     start_concentration = 2000000
     dilution_ratio = 2
     number_dilutions = 11
+    x_label = 'Log_X'
+    y_label = 'RLU'
+    title = 'Dose Response'
 
-    def graph(self):
+    def type(self):
         return 'DRC'
 
 
@@ -133,6 +159,10 @@ class Inhibition(Nab):
     start_concentration = 1000
     dilution_ratio = 2
     number_dilutions = 10
+    x_label = 'Log_X'
+    y_label = '%Inhibition'
+    title = 'Inhibition'
 
-    def graph(self):
+    def type(self):
         return 'inhibition'
+
